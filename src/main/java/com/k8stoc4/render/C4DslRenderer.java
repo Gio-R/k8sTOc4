1b798f76f1572aa8692b5e59983d666b63596045
Alex Speranza <asperanza@imolainformatica.it>
2026-03-09 14:47:34 +0100

    refactor: remove unused method

--- src/main/java/com/k8stoc4/render/C4DslRenderer.java
+++ src/main/java/com/k8stoc4/render/C4DslRenderer.java
@@ -1,20 +1,14 @@
 package com.k8stoc4.render;
 
-import com.github.mustachejava.DefaultMustacheFactory;
-import com.github.mustachejava.Mustache;
-import com.github.mustachejava.MustacheFactory;
 import com.k8stoc4.presenter.C4NamespacePresenter;
 import lombok.extern.slf4j.Slf4j;
 import com.k8stoc4.model.*;
 
-import java.io.StringWriter;
 import java.util.*;
 import java.util.stream.Collectors;
 
 @Slf4j
 public class C4DslRenderer {
-
-    private static final MustacheFactory MF = new DefaultMustacheFactory();
 
     public Output render(final C4Model model) {
         return new Output(renderModel(model), renderSpec(model));
@@ -88,87 +82,6 @@ public class C4DslRenderer {
         }
 
         return sb.toString();
-    }
-
-    private String renderNamespace(C4Namespace namespace) {
-        Mustache mustache = MF.compile("templates/namespace.mustache");
-
-        List<Map<String, Object>> labelGroups = new ArrayList<>();
-        for (C4LabelGroup lg : namespace.getLabelGroups()) {
-            Map<String, Object> lgModel = new HashMap<>();
-            lgModel.put("name", lg.getName().replace(".","-"));
-            lgModel.put("components", lg.getComponents().stream()
-                .map(c -> {
-                    Map<String, Object> model = new HashMap<>();
-                    model.put("kind", c.getKind().toLowerCase());
-                    model.put("id", c.getId().replace(".", "-"));
-                    model.put("name", c.getName());
-                    model.put("technology", c.getKind());
-                    model.put("description", c.getDescription());
-                    model.put("labels", Optional.ofNullable(c.getLabels()).orElse(Map.of()).entrySet().stream()
-                            .map(e -> Map.of("key", e.getKey(), "value", e.getValue()))
-                            .toList());
-                    model.put("annotations", Optional.ofNullable(c.getAnnotations()).orElse(Map.of()).entrySet().stream()
-                            .map(e -> Map.of("key", e.getKey(), "value", e.getValue()))
-                            .toList());
-                    return model;
-                })
-                .toList());
-            labelGroups.add(lgModel);
-        }
-
-        List<Map<String, Object>> comps =
-                namespace.getComponents().stream()
-                        .map(c -> {
-                            Map<String, Object> model = new HashMap<>();
-
-                            model.put("kind", c.getKind().toLowerCase());
-                            model.put("id", c.getId().replace(".", "-"));
-                            model.put("name", c.getName());
-                            model.put("technology", c.getKind());
-                            model.put("description", c.getDescription());
-
-                            model.put(
-                                    "labels",
-                                    Optional.ofNullable(c.getLabels()).orElse(Map.of()).entrySet().stream()
-                                            .map(e -> Map.of(
-                                                    "key", e.getKey(),
-                                                    "value", e.getValue()
-                                            ))
-                                            .toList()
-                            );
-
-                            model.put(
-                                    "annotations",
-                                    Optional.ofNullable(c.getAnnotations()).orElse(Map.of()).entrySet().stream()
-                                            .map(e -> Map.of(
-                                                    "key", e.getKey(),
-                                                    "value", e.getValue()
-                                            ))
-                                            .toList()
-                            );
-
-                            return model;
-                        })
-                        .toList();
-
-
-        List<String> relations = new ArrayList<>();
-
-        for (C4Relationship rel : namespace.getRelationships()) {
-            relations.add(rel.getSource() + " -> " + rel.getTarget()+" "+rel.getTag());
-        }
-
-        Map<String, Object> ctx = Map.of(
-                "name", namespace.getName(),
-                "labelGroups", labelGroups,
-                "components", comps,
-                "relations", relations
-        );
-
-        StringWriter writer = new StringWriter();
-        mustache.execute(writer, ctx);
-        return writer.toString();
     }
 
     public String renderRelations(C4Model model) {
